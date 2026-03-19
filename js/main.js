2657af12d9df2c307d2e30baa018bf07855fa652
Leti <letivsan@gmail.com>
2026-03-19 13:56:53 +0100

    added new functions

--- js/main.js
+++ js/main.js
@@ -12,23 +12,28 @@ const yearElement = document.querySelector ('.js-year');
 const resultSection = document.querySelector ('.js-result');
 
 /* SECCIÓN DE DATOS */
+// variables que cambian durante ejecución
 let userScore = 0;
 let computerScore = 0;
 let rounds = 0;
 
+// reglas 
 const rules = {
   piedra: 'tijera',
   papel: 'piedra',
   tijera: 'papel'
 };
 
+// año automático
 const currentYear = new Date().getFullYear();
 
 /* SECCIÓN DE FUNCIONES */
+// número aleatorio
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+// jugada ordenador
 function getComputerMove() {
   const randomNumber = getRandomNumber(9);
 
@@ -41,12 +46,32 @@ function getComputerMove() {
   }
 }
 
+// actualizar html y pintar las puntuaciones
 function renderScores() {
   userScoreElement.textContent = userScore;
   computerScoreElement.textContent = computerScore;
   roundCounterElement.textContent = rounds;
 }
 
+//pintar la jugada
+function renderPlay (userMove, computerMove, message) {
+  resultParagraph.textContent = `Jugadora: ${userMove} | Computadora: ${computerMove} → ${message}`;
+}
+
+// lógica del resultado
+function renderMessage (userMove, computerMove, winMessage, loseMessage, drawMessage) {
+  if (userMove === computerMove) {
+    return drawMessage;
+  } else if (rules [userMove] === computerMove) {
+    userScore++;
+    return winMessage;
+  } else {
+    computerScore++;
+    return loseMessage;
+  }
+}
+
+// decide ganador final
 function getWinnerMessage() {
   if (userScore > computerScore) {
     return ' 🏆¡Has ganado la partida!';
@@ -57,41 +82,64 @@ function getWinnerMessage() {
   }
 }
 
+//mostrar la sección de resultados
+function showResultSection () {
+  resultSection.classList.remove ('hidden');
+}
+
+// fin del juego
 function endGame() {
   resultParagraph.textContent = getWinnerMessage();
 
+  // ocultar jugar y muestra reiniciar
   btnPlay.classList.add('hidden');
   btnRestart.classList.remove('hidden');
 }
 
+// mostrar resultados con sección oculta inicio
 function playGame() {
   if (rounds === 0) {
-    resultSection.classList.remove ('hidden');
+    // resultSection.classList.remove ('hidden');
+    showResultSection ();
   }
 
+  // comprobar limite y salir con return. Si ya terminó llama a endgame
   if (rounds >= 10) {
     endGame();
     return;
   }
 
+  // obtener jugadas
   const userMove = selectMove.value;
   const computerMove = getComputerMove();
 
-  let message = '';
+ 
+  // let message = '';
+  // // lógica del juego
+  // if (userMove === computerMove) {
+  //   message = 'Empate';
+  // } else if (rules[userMove] === computerMove) {
+  //   message = '¡Has ganado!';
+  //   userScore++;
+  // } else {
+  //   message = '¡Has perdido!';
+  //   computerScore++;
+  // }
 
-  if (userMove === computerMove) {
-    message = 'Empate';
-  } else if (rules[userMove] === computerMove) {
-    message = '¡Has ganado!';
-    userScore++;
-  } else {
-    message = '¡Has perdido!';
-    computerScore++;
-  }
-
+ const message = renderMessage(
+  userMove,
+  computerMove,
+  '¡Has ganado!',
+  '¡Has perdido!',
+  'Empate'
+ );
+ 
   rounds++;
 
-  resultParagraph.textContent = `Jugadora: ${userMove} | Computadora: ${computerMove} → ${message}`;
+  renderPlay (userMove, computerMove, message);
+
+  // muestra resultado en texto
+  // resultParagraph.textContent = `Jugadora: ${userMove} | Computadora: ${computerMove} → ${message}`;
 
   renderScores();
 
@@ -100,6 +148,7 @@ function playGame() {
   }
 }
 
+// reinicio estado
 function resetGame() {
   userScore = 0;
   computerScore = 0;
@@ -121,10 +170,12 @@ function handleClickPlay(ev) {
   playGame();
 }
 
+// reinicio
 function handleClickRestart() {
   resetGame();
 }
 
+// conexión botón con función
 btnPlay.addEventListener('click', handleClickPlay);
 btnRestart.addEventListener('click', handleClickRestart);
 
